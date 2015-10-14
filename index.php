58b95b439f8112d34e549e565bb80e9600535038
Anton <anton.bil@hz.nl>
2015-10-14 14:26:05 +0200

    added ordernr field, added services askstartinggames,initiategame

--- index.php
+++ index.php
@@ -1,54 +1,6 @@
 <?php
 // //created september 2015 by anton bil
-require 'vendor/slim/slim/Slim/Slim.php';
-\Slim\Slim::registerAutoloader();
-// create new Slim instance
-$app = new \Slim\Slim();
-//propel is een alternatief voor notorm
-//put voor nieuwe en update
-//syncen van namen velden en api-parameters
-$db=getDB();
-// add new Route 
-$app->get("/", function () {
-    echo "<h1>Hello Slim World</h1>";
-});//
-//identify(ip,naam)
-$app->get('/identify/:ip/:naam',function ($ip,$naam) use ($app, $db) {   
-    $findplayer=$db->player->where("ip", $ip);
-//var_dump($findplayer);
-    if (count($findplayer)>0){
-     $players = array();
-     foreach ($findplayer as $player) {
-        $players[]  = array(
-            "id" => $player["id"],
-            "ip" => $player["ip"],
-            "name" => $player["name"],
-            "status" => $player["status"]
-        );
-      }
-     } else {
-	$id=$_SERVER['REMOTE_ADDR'];
-	$status='ok';
-        $newplayer=array(
-	  "id" => $id, // omit or null for auto_increment
-	  "ip" => $ip,
-	  "name" => $naam,
-	  "status" => $status,
-	);
-	$result = $db->player->insert($newplayer);
-        $players=$result;
-    }
-    $app->response()->header("Content-Type", "application/json");
-    echo json_encode($players);
-   $db=null;
-});
-$app->get('/askstartinggames/:ip',function ($ip) use ($app, $db) { 
-});
-// run the Slim app
-$app->run();
-
-function getDB()
-{
+//db structure:
 /*$db = new SQLite3('mysqlitedb.db');*/
 /*game(gamenumber,status,starter,tokenplayer,deckontable,restcards,winner)
 gameuser(usernr,gamenumber,status)
@@ -85,6 +37,93 @@ player STRING,
 time STRING,
 cards STRING,
 PRIMARY KEY (game, player,time));');*/
+
+require 'vendor/slim/slim/Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
+// create new Slim instance
+$app = new \Slim\Slim();
+//propel is een alternatief voor notorm
+//put voor nieuwe en update
+//syncen van namen velden en api-parameters
+$db=getDB();
+// add new Route 
+$app->get("/", function () {
+    echo "<h1>Hello Slim World</h1>";
+});//
+//identify(ip,naam)
+//curl  -X POST http://127.0.0.1/anton/cardapi/identify/myip/anton
+//of
+//curl -H "Content-Type: application/json" -X POST -d '{"username":"xyz","password":"xyz"}' http://127.0.0.1/anton/cardapi/identify/myip/hans
+$app->post('/identify/:ip/:naam',function ($ip,$naam) use ($app, $db) {   
+    $findplayer=$db->player->where("ip", $ip);
+//var_dump($findplayer);
+    if (count($findplayer)>0){
+     $players = array();
+     foreach ($findplayer as $player) {
+        $players[]  = array(
+            "id" => $player["id"],
+            "ip" => $player["ip"],
+            "name" => $player["name"],
+            "status" => $player["status"]
+        );
+      }
+     } else {
+	$id=$_SERVER['REMOTE_ADDR'];
+	$status='ok';
+        $newplayer=array(
+	  "id" => $id, // omit or null for auto_increment
+	  "ip" => $ip,
+	  "name" => $naam,
+	  "status" => $status,
+	);
+	$result = $db->player->insert($newplayer);
+        $players=$result;
+    }
+    returnResult($players);
+   $db=null;
+});
+
+$app->post('/initiategame/:ip',function ($ip) use ($app, $db) {  
+    $nr=$db->game->max("gamenumber");
+	$status='initiated';
+        $newgame=array(
+	  "gamenumber" => $nr+1,
+	  "starter" => $ip,
+	  "tokenplayer" => 1,
+	  "status" => $status
+	);
+	$result = $db->game->insert($newgame);
+    returnResult($result);
+});
+//curl -X POST http://127.0.0.1/anton/cardapi/initiategame/myip2
+$app->get('/askstartinggames/:ip',function ($ip) use ($app, $db) { 
+    $findgames=$db->gameuser->where("player", $ip);
+    if (count($findgames)>0){
+     $games = array();
+     foreach ($findgames as $game) {
+        $games[]  = array(
+            "id" => $game["game"],
+            "player" => $game["player"]
+        );
+      }
+
+    returnResult($games);
+      }
+});
+// run the Slim app
+$app->run();
+
+/*
+return result as JSON-object
+*/
+function returnResult($result){
+    $app->response()->header("Content-Type", "application/json");
+	echo json_encode($result);
+}
+
+function getDB()
+{
+
 require "notorm/NotORM.php";
 
 $pdo = new PDO('sqlite:mysqlitedb.db');
