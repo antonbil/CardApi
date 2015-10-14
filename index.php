cedb250fc9b59bf00c7c72192e519a10ec06960f
Anton <anton.bil@hz.nl>
2015-10-14 17:04:19 +0200

    added stubs for a lot of services

--- index.php
+++ index.php
@@ -38,6 +38,8 @@ time STRING,
 cards STRING,
 PRIMARY KEY (game, player,time));');*/
 
+global $GAME_STATE;
+$GAME_STATE = ['initiated', 'running','ended'];
 require 'vendor/slim/slim/Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
 // create new Slim instance
@@ -85,7 +87,7 @@ $app->post('/identify/:ip/:naam',function ($ip,$naam) use ($app, $db) {
 
 $app->post('/initiategame/:ip',function ($ip) use ($app, $db) {  
     $nr=$db->game->max("gamenumber");
-	$status='initiated';
+	$status=$GLOBALS['GAME_STATE'][0];//'initiated';
         $cards=array();
         for ($i=1;$i<53;$i++)$cards[]=$i;
         $newgame=array(
@@ -110,7 +112,7 @@ $app->post('/initiategame/:ip',function ($ip) use ($app, $db) {
 });
 //curl -X POST http://127.0.0.1/anton/cardapi/initiategame/myip2
 $app->get('/askstartinggames/:ip',function ($ip) use ($app, $db) { 
-    $findgames=$db->gameuser->where("player", $ip);
+    $findgames=$db->gameuser->where("status", $GLOBALS['GAME_STATE'][0]);
     if (count($findgames)>0){
      $games = array();
      foreach ($findgames as $game) {
@@ -123,6 +125,67 @@ $app->get('/askstartinggames/:ip',function ($ip) use ($app, $db) {
     returnResult($app,$games);
       }
 });
+//returns list of ip for all players who have applied for a game
+$app->get('/askdatastartinggame/:ip/:gamenr',function ($ip, $gamenr) use ($app, $db) { 
+});
+
+
+//returns name of ipplayer if ip is known to system, otherwise -1
+$app->get('/getplayerinfo/:ip/:ipplayer',function ($ip, $ipplayer) use ($app, $db) { 
+});
+
+//returns 1 if game still starting, and not full, and ip has not applied for this game yet. Otherwise 0.
+$app->get('/applyforgame/:ip/:gamenr',function ($ip, $gamenr) use ($app, $db) { 
+});
+
+//returns 1 if game is started, 0 if ip has not initiated game or no players yet
+$app->get('/startgame/:ip/:gamenr',function ($ip, $gamenr) use ($app, $db) { 
+});
+//returns the ip which has the token for the game, 0 if game not started yet etc.
+$app->get('/gettoken/:ip/:gamenr',function ($ip, $gamenr) use ($app, $db) { 
+});
+//returns the cards a player has (array of cardnumbers) game is still playing, 0 otherwise 
+$app->get('/gethand/:ip/:gamenr',function ($ip, $gamenr) use ($app, $db) { 
+});
+//returns cardnumber which player gets if player has token, 0 otherwise
+$app->get('/getcard/:ip/:gamenr',function ($ip, $gamenr) use ($app, $db) { 
+});
+//returns 1 if ip has token, 0 otherwise.
+$app->get('/pushcard/:ip/:gamenr/:cardnumber',function ($ip, $gamenr, $cardnumber) use ($app, $db) { 
+});
+//-getexchange(ip,ipplayer,gamenr) get
+//return cardnumber last pushed of player
+$app->get('/getexchange/:ip/:gamenr/:ipplayer',function ($ip, $gamenr, $ipplayer) use ($app, $db) { 
+});
+//-getdeckontable(ip,gamenr) get
+//returns list of cardnumbers lying open to see
+$app->get('/getdeckontable/:ip/:gamenr',function ($ip, $gamenr) use ($app, $db) { 
+});
+//-swapcards(ip,gamenr) post
+//returns list of cards lying on table if player has token, otherwise error
+$app->get('/swapcards/:ip/:gamenr',function ($ip, $gamenr) use ($app, $db) { 
+});
+//-offerpass(ip,gamenr) post
+//returns 1 if player has token, error otherwise
+$app->get('/offerpass/:ip/:gamenr',function ($ip, $gamenr) use ($app, $db) { 
+});
+//-claimwin(ip,gamenr) post
+//returns 1 if player has won, and has token. error otherwise
+$app->get('/claimwin/:ip/:gamenr',function ($ip, $gamenr) use ($app, $db) { 
+});
+//-getstategame(ip,gamenr) get
+//returns one of: initiated,started,ended
+$app->get('/getstategame/:ip/:gamenr',function ($ip, $gamenr) use ($app, $db) { 
+});
+//-getresultgame(ip,gamenr, ipplayer) get
+//returns list of cards for ipplayer if game is ended.
+$app->get('/getresultgame/:ip/:gamenr:/ipplayer',function ($ip, $gamenr, $ipplayer) use ($app, $db) { 
+});
+//-getwinnergame(ip,gamenr) get
+//returns ip of winning player if game is ended
+$app->get('/getwinnergame/:ip/:gamenr',function ($ip, $gamenr) use ($app, $db) { 
+});
+
 // run the Slim app
 $app->run();
 
