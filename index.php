a87199a0eab8ce2ef7971d4fded5ae3bd2866b51
Anton <anton.bil@hz.nl>
2015-10-30 00:24:00 +0100

    player cards can now be fetched for any player if game is ended

--- index.php
+++ index.php
@@ -627,7 +627,7 @@ $app->get('/games/:ip/:gamenr/state',function ($ip, $gamenr) use ($app) {
 //returns list of cards for ipplayer if game is ended.
 $app->get('/games/:ip/:gamenr/cards/:ipplayer',function ($ip, $gamenr, $ipplayer) use ($app) {
 	$gameplayer=$app->checkgameplayertoken($ipplayer, $gamenr,$app->gameState(CardApi::ENDED),false);
-	if (!$gameplayer){$app->returnError("game $gamenr not ended or $ipplayer does not have token");return;};
+	if (!$gameplayer){return;};
 	//echo "player cards can be shown!";
 	$cards=$gameplayer["finduser"]["cards"];
 	$app->returnResult($gameplayer["finduser"]);
