c04fffe02afe13d54a114e11354ca325f9d81fdf
Anton <anton.bil@hz.nl>
2016-01-13 11:50:59 +0100

    fix syntax error

--- index.php
+++ index.php
@@ -908,10 +908,10 @@ $app->get('/games/:ip/gamewinners',function ($ip) use ($app) {
 	    else
 	      $winners[$game[1]]=1;
 	  }
-	  $result=array();
+	  /*$result=array();
 	  foreach ($winners as $winner) {
 	    result[]=array($winner[0]=>$winner[1]);
-	  }
+	  }*/
 	} else {$app->returnError("no games available");return;}
 	$app->returnResult(array(
             "result" => $winners));
